1560c5a6c279360e51975627a59a85cf5e38b132
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-09-02 11:52:37 -0500

    Formulario HTML en PHP, MVC y POO

---
+++ views/persona/editar-persona.php
@@ -0,0 +1,33 @@
+<?php
+require "../template/header.php";
+?>
+
+<main class="container">
+    <h1 class="text-center">Editar Persona</h1>
+    <a href="<?= BASE_URL ?>/views/persona">Listar Personas</a>
+
+    <br><br>
+    <form>
+        <div class="mb-3">
+            <label for="txtNombre" class="form-label">Nombres</label>
+            <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombres" required>
+        </div>
+        <div class="mb-3">
+            <label for="txtApellido" class="form-label">Apellidos</label>
+            <input type="text" class="form-control" name="txtApellido" id="txtApellido" placeholder="Apellidos" required>
+        </div>
+        <div class="mb-3">
+            <label for="txtTelefono" class="form-label">Teléfono</label>
+            <input type="number" class="form-control" name="txtTelefono" id="txtTelefono" placeholder="Número de teléfono" required>
+        </div>
+        <div class="mb-3">
+            <label for="txtEmail" class="form-label">Email</label>
+            <input type="mail" class="form-control" name="txtEmail" id="txtEmail" placeholder="Correo electrónico" required>
+        </div>
+        <button type="submit" class="btn btn-info"><i class="fa-solid fa-pen"></i> Actualizar</button>
+    </form>
+</main>
+
+<?php
+require "../template/footer.php";
+?>
--- views/persona/index.php
+++ views/persona/index.php
@@ -4,7 +4,7 @@ require "../template/header.php";
 
 <main class="container">
     <h1 class="text-center">Lista de Personas</h1>
-    <a href="crear.php" class="btn btn-success">Crear Persona</a>
+    <a href="crear-persona.php" class="btn btn-success"><i class="fa-solid fa-user-plus"></i> Crear Persona</a>
 
     <table id="tblPersonas" class="table table-striped">
         <thead>
@@ -25,7 +25,7 @@ require "../template/header.php";
                 <td>4567890</td>
                 <td>mark@info.com</td>
                 <td>
-                    <a href="" class="btn btn-outline-primary btn-sm" title="Eliminar registro"><i class="fa-solid fa-user-pen"></i></a>
+                    <a href="<?= BASE_URL ?>views/persona/editar-persona.php?p=1" class="btn btn-outline-primary btn-sm" title="Eliminar registro"><i class="fa-solid fa-user-pen"></i></a>
                     <button class="btn btn-outline-danger btn-sm" title="Eliminar registro"><i class="fa-solid fa-trash"></i></button>
                 </td>
             </tr>
